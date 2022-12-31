1f4d7736241d2cfcdcf4f1027fc818fea69dea07
Murtaza <murtaza.mahdiyar@gmail.com>
2022-12-31 06:27:09 +0430

    updated content

---
+++ src/app/shared/modal/modal.component.ts
@@ -0,0 +1,27 @@
+import { Component, Input, ElementRef, OnInit } from '@angular/core';
+import { ModalService } from 'src/app/services/modal.service';
+
+@Component({
+  selector: 'app-modal',
+  templateUrl: './modal.component.html',
+  styleUrls: ['./modal.component.css'],
+  // providers: [
+  //   ModalService
+  // ]
+})
+export class ModalComponent implements OnInit {
+  @Input() modalID = ''
+  constructor(
+    public modal: ModalService,
+    public el: ElementRef
+  ){}
+
+  closeModal(){
+    this.modal.toggleModal(this.modalID)
+  }
+
+  ngOnInit(){
+    document.body.appendChild(this.el.nativeElement)
+  }
+
+}
